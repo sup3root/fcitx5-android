7b359da1be3b15304b276a34c265fc239d2331ec
sup3r <fcitx5-android@sup3r.me>
2025-12-10 17:42:57 +0800

    popup: customize popup symbols
    ci: install Android platform-tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,17 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+if (keystorePropertiesFile.exists()) {
+    FileInputStream(keystorePropertiesFile).use {
+        keystoreProperties.load(it)
+    }
+} else {
+    println("WARNING: keystore.properties file not found. Release signing config will be incomplete.")
+}
+
 plugins {
     id("org.fcitx.fcitx5.android.app-convention")
     id("org.fcitx.fcitx5.android.native-app-convention")
@@ -10,6 +24,15 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = keystoreProperties.getProperty("STORE_FILE")?.let { file(it) }
+            storePassword = keystoreProperties.getProperty("STORE_PASSWORD")
+            keyAlias = keystoreProperties.getProperty("KEY_ALIAS")
+            keyPassword = keystoreProperties.getProperty("KEY_PASSWORD")
+        }
+    }
+
     namespace = "org.fcitx.fcitx5.android"
 
     defaultConfig {
